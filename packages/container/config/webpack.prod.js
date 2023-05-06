d6150fd38f14e1e3c6806000cf285891a4d0cf67
Prayush Dave <prayush@kristal.ai>
2023-05-06 18:30:36 +0530

    Add public Path to the prodConfig

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,17 +1,18 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const { merge } = require("webpack-merge");
-const packageJson = require("./../package.json");
-const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { merge } = require('webpack-merge');
+const packageJson = require('./../package.json');
+const commonConfig = require('./webpack.common');
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[contenthash].js",
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
