fd8482a871c627a5d074fff583c3ce6856aa69fd
Prayush Dave <prayush@kristal.ai>
2023-05-21 17:59:25 +0530

    Re-Add auth config

---
+++ packages/auth/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('./../package.json');
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:9002/', //Need to set it because else the the main.js made by webpack will be lost at some other place.
+  },
+  devServer: {
+    port: 9002,
+    historyApiFallback: {
+      index: '/index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
